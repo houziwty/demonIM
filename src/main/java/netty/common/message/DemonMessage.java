b1ff91419c682efec1c2c856588d7cfcc853d49a
Demon <houziwty@126.com>
2015-06-26 13:56:10 +0800

    update
    update

--- src/main/java/netty/common/message/DemonMessage.java
+++ src/main/java/netty/common/message/DemonMessage.java
@@ -11,10 +11,25 @@ public class DemonMessage implements Serializable {
 	 */
 	private static final long serialVersionUID = 5471528125749414349L;
 	
-	private DemonLinkedList<DemonHeader>_hdeaders;
+	private DemonLinkedList<DemonHeader>_headers;
 	private DemonLinkedList<DemonBody>_boyds;
 	private DemonMessageType _messageType;
 	private byte _method;
     private transient DemonTransaction _parentTrans;
+    public DemonHeader From;
+    public DemonHeader To;
+    public DemonHeader CallId;
+    public DemonHeader Casequence;
+    public DemonHeader Fpid;
+    public DemonHeader Tpid;
+    public DemonHeader Event;
+    
+    public DemonMessage(byte method){
+       this._method=method;
+       this._messageType=((this._method|0x7F)==0x7F)?DemonMessageType.Request:DemonMessageType.Response;
+       this._headers=new DemonLinkedList<DemonHeader>();
+       this._boyds=new DemonLinkedList<DemonBody>();
+    }
+    
 
 }
