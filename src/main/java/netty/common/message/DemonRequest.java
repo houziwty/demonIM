934f147c36edd0064af0e623a261344e1149d63e
Demon <houziwty@126.com>
2015-07-13 16:24:09 +0800

    update
    update

--- src/main/java/netty/common/message/DemonRequest.java
+++ src/main/java/netty/common/message/DemonRequest.java
@@ -1,17 +1,45 @@
 package netty.common.message;
 
-
+import java.util.ArrayList;
 
 public class DemonRequest extends DemonMessage implements Cloneable {
-
-	public DemonRequest(byte method) {
-		super(method);
-		// TODO Auto-generated constructor stub
-	}
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
+	public DemonRequest(byte method) {
+		super(method);
+		// TODO Auto-generated constructor stub
+	}
+
+	public byte getMethod() {
+		return super.getMethodValue();
+	}
+
+	public boolean isMethod(byte method) {
+		return method == super.getMethodValue();
+	}
+
+	public DemonRequest clone() {
+		DemonRequest request = new DemonRequest(this.getMethodValue());
+		ArrayList<DemonHeader> list = this.getHeaders();
+		ArrayList<DemonBody> bodys = this.getBodys();
+		for (DemonHeader header : list) {
+			if (!(header.isTypeOf(DemonHeaderType.RecordRoute) || header
+					.isTypeOf(DemonHeaderType.Route))) {
+				request.addHeader(new DemonHeader(header.getType(), header
+						.getValue()));
+			}
+		}
+		for (DemonBody body : bodys) {
+			request.addBody(new DemonBody(body.getValue()));
+		}
+		return request;
+	}
+
+	public void setMethod(byte method) {
+		super.setMethod(method);
+	}
 }
