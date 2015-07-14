82d6885b9c16960644816e659a3f8906074de602
Demon <houziwty@126.com>
2015-07-14 11:22:35 +0800

    update
    updete

--- src/main/java/netty/common/message/DemonResponse.java
+++ src/main/java/netty/common/message/DemonResponse.java
@@ -2,14 +2,46 @@ package netty.common.message;
 
 public class DemonResponse extends DemonMessage implements Cloneable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5021522053950213638L;
+
 	public DemonResponse(byte method) {
 		super(method);
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+	public DemonResponse(DemonRequest request) {
+		this(request, DemonResponseCode.OK);
+	}
+
+	public DemonResponse(DemonRequest request, byte ok) {
+		super(ok);
+		for (DemonHeader header : request.getHeaders()) {
+			if (header != null) {
+				switch (header.getType()) {
+				case DemonHeaderType.From:
+				case DemonHeaderType.To:
+				case DemonHeaderType.CallId:
+				case DemonHeaderType.Csequence:
+				case DemonHeaderType.Fpid:
+				case DemonHeaderType.Tpid:
+					this.addHeader(header.clone());
+					break;
+				default:
+					break;
+				}
+			}
+		}
+	}
+
+	public byte getStatusCode() {
+		return super.getMethodValue();
+	}
+
+	public boolean isResponseCode(byte code) {
+		return super.getMethodValue() == code;
+	}
 
 }
