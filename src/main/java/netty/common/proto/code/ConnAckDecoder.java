895e96634986de55dc3a76523a7fc66998c925c6
wangtianyu <houziwty@126.com>
2017-03-11 18:27:33 +0800

    update序列化

---
+++ src/main/java/netty/common/proto/code/ConnAckDecoder.java
@@ -0,0 +1,32 @@
+package netty.common.proto.code;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.util.AttributeMap;
+import netty.common.proto.message.ConnAckMessage;
+
+import java.util.List;
+
+/**
+ *
+ * @author andrea
+ */
+class ConnAckDecoder extends DemuxDecoder {
+
+    @Override
+    void decode(AttributeMap ctx, ByteBuf in, List<Object> out) throws Exception {
+        in.resetReaderIndex();
+        //Common decoding part
+        ConnAckMessage message = new ConnAckMessage();
+        if (!decodeCommonHeader(message, 0x00, in)) {
+            in.resetReaderIndex();
+            return;
+        }
+        //skip reserved byte
+        in.skipBytes(1);
+        
+        //read  return code
+        message.setReturnCode(in.readByte());
+        out.add(message);
+    }
+    
+}
