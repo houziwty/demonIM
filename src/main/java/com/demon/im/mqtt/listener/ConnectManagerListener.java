ef04cc9456541926e1ad6e7da2df987085acb20f
DemonWang <houziwty@126.com>
2017-04-25 17:57:36 +0800

    修改超时时间

--- src/main/java/com/demon/im/mqtt/listener/ConnectManagerListener.java
+++ src/main/java/com/demon/im/mqtt/listener/ConnectManagerListener.java
@@ -35,7 +35,7 @@ public class ConnectManagerListener implements MQTTServerListener {
 
     @Override
     public void disconnected(DisconnectMessage msg, ChannelHandlerContext ctx) throws Exception {
-     
+
     }
 
     @Override
