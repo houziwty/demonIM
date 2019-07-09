8078e4295b4e0d9a5a71142bf85ca766cc2e3212
天宇 <tianyu.wang@moji.com>
2019-07-09 18:27:52 +0800

    添加日志
    添加日志

--- src/main/java/com/demon/im/websocket/WebSocketServer.java
+++ src/main/java/com/demon/im/websocket/WebSocketServer.java
@@ -1,6 +1,7 @@
 package com.demon.im.websocket;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
@@ -15,35 +16,30 @@ import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.stream.ChunkedWriteHandler;
 
 public class WebSocketServer {
+	public static final Logger LOGGER=LoggerFactory.getLogger(WebSocketServer.class);
+	
+	
 	public void run(int port) {
 		EventLoopGroup bossGroup = new NioEventLoopGroup();
 		EventLoopGroup workerGroup = new NioEventLoopGroup();
 		try {
 			ServerBootstrap b = new ServerBootstrap();
-			b.group(bossGroup, workerGroup)
-					.channel(NioServerSocketChannel.class)
+			b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
 					.childHandler(new ChannelInitializer<SocketChannel>() {
 
-						protected void initChannel(SocketChannel ch)
-								throws Exception {
+						protected void initChannel(SocketChannel ch) throws Exception {
 							ChannelPipeline pipeline = ch.pipeline();
-							pipeline.addLast("http-codec",
-								   new HttpServerCodec());
-							pipeline.addLast("aggregator",
-									new HttpObjectAggregator(65536));
-							ch.pipeline().addLast("http-chunked",
-									 new ChunkedWriteHandler());
-							pipeline.addLast("handler",
-									new WebSocketServerHandler());
+							pipeline.addLast("http-codec", new HttpServerCodec());
+							pipeline.addLast("aggregator", new HttpObjectAggregator(65536));
+							ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+							pipeline.addLast("handler", new WebSocketServerHandler());
 
 						}
 
 					});
 			Channel ch = b.bind(port).sync().channel();
 			System.out.println("Web socket server started at port " + port);
-			System.out
-					.println("Open your browser and navigate to http://localhost:"
-							+ port);
+			System.out.println("Open your browser and navigate to http://localhost:" + port);
 			ch.closeFuture().sync();
 
 		} catch (Exception ex) {
@@ -60,7 +56,7 @@ public class WebSocketServer {
 		int port = 8080;
 		if (args.length > 0) {
 			try {
-				port=Integer.parseInt(args[0]);
+				port = Integer.parseInt(args[0]);
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
